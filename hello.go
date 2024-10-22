92e698f913050fa24de0bdadea2fbaa924831a01
Minh Huyen <ttminhuyen1111@gmail.com>
2024-10-22 11:17:38 +0700

    push

--- hello.go
+++ hello.go
@@ -8,4 +8,12 @@ import (
 
 func main() {
 	fmt.Println(quote.Go())
+	fmt.Println(Hello("Gladys"))
+}
+
+// Hello returns a greeting for the named person.
+func Hello(name string) string {
+	// Return a greeting that embeds the name in a message.
+	message := fmt.Sprintf("Hi, %v. Welcome!", name)
+	return message
 }
